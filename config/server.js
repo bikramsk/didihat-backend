bc9662d35fd11e12e896d60364815190caf9d47b
bikramsk <bikramkanyal@gmail.com>
2025-09-04 09:43:47 +0530

    new changes added

--- config/server.js
+++ config/server.js
@@ -1,46 +1,25 @@
-
 module.exports = ({ env }) => ({
-  url: env('NODE_ENV') === 'development' 
-    ? 'http://localhost:1350'
-    : 'https://admin.didihat.com',
-
-  host: '0.0.0.0',
+  host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1350),
-
+  url: env('PUBLIC_URL', 'http://localhost:1350'),
   app: {
     keys: env.array('APP_KEYS'),
   },
-
-  admin: {
-    url: '/admin',
-    serveAdminPanel: true,
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+  },
+  middleware: {
+    settings: {
+      cors: {
+        enabled: true,
+        origin: ['*'],
+        headers: ['*'],
+        methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'],
+        keepHeaderOnError: true,
+      },
+    },
   },
 });
-
-
-// module.exports = ({ env }) => ({
-//   host: env('HOST', '0.0.0.0'),
-//   port: env.int('PORT', 1337),
-//   url: env('PUBLIC_URL', 'http://localhost:1337'),
-//   app: {
-//     keys: env.array('APP_KEYS'),
-//   },
-//   webhooks: {
-//     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
-//   },
- 
-//   middleware: {
-//     settings: {
-//       cors: {
-//         enabled: true,
-//         origin: ['*'],
-//         headers: ['*'],
-//         methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'],
-//         keepHeaderOnError: true,
-//       },
-//     },
-//   },
-// });
 
 // module.exports = ({ env }) => ({
 //   host: env('HOST', '0.0.0.0'),
